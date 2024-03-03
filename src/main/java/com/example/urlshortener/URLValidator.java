1909d0d3ae32efe0b6c152fa417ff81379f5854d
ankitdani <ankitdani1997@gmail.com>
2024-03-02 21:54:01 -0800

    Simplified the regular expression to validate url in the URLValidator file

--- src/main/java/com/example/urlshortener/URLValidator.java
+++ src/main/java/com/example/urlshortener/URLValidator.java
@@ -6,7 +6,9 @@ import java.util.regex.Pattern;
 
 public class URLValidator {
     public static final URLValidator INSTANCE = new URLValidator();
-    private static final String URL_REGEX = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$";
+//    private static final String URL_REGEX = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$";
+    private static final String URL_REGEX = "^(http|https):\\/\\/[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(\\/.*)?$|^([a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(\\/.*)?)$";
+
 
     private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
 
